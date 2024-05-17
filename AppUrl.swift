a34a170ea1968fba9b3b1b5d333335de465b8385
Ali Syed <alisyedali000@gmail.com>
2024-05-18 01:51:27 +0500

    Network layer added

---
+++ AppUrl.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  CustomNetworkManagerMLB
+//
+//  Created by Ali Syed on 18/05/2024.
+//
+
+import Foundation
+
+enum AppUrl {
+    
+    static var SCHEME = "https"
+    static var BASEURL = ""
+    static var APIPATCH = "/"
+    static var IMAGEURL = "https://image.tmdb.org/t/p/original"
+
+}
+
--- File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  CustomNetworkManagerMLB
-//
-//  Created by Ali Syed on 23/04/2024.
-//
-
-import Foundation
